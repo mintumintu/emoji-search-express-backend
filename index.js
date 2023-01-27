a279f9191471cbb54dbfadc39df26be95210f79a
Jogendra Pattnaik <jogendrapattnaik4@gmail.com>
2023-01-27 20:23:23 +0530

    initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app= express();
+
+app.get('/',(req,res)=>{
+    res.status(200).send("<h1>Hello</h1>");
+})
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    app.listen(process.env.PORT,(req,res)=>{
+        console.log("Connected to Server & mongodb");
+    })
+}).catch((err)=>{
+    console.log(err);
+})
